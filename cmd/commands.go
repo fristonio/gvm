06d73241cae3bbe1e38def74f33a85bc030adc91
Deepesh Pathak <deepshpathak@gmail.com>
2018-04-21 22:25:27 +0530

    feat(editorconfig): add editorconfig

--- cmd/commands.go
+++ cmd/commands.go
@@ -1,11 +1,11 @@
 package cmd
 
 import (
-  "fmt"
-  "os"
+	"fmt"
+	"os"
 
-  "github.com/fristonio/gvm/logger"
-  "github.com/spf13/cobra"
+	"github.com/fristonio/gvm/logger"
+	"github.com/spf13/cobra"
 )
 
 var longDescriptionGvm = `
@@ -16,22 +16,22 @@ Complete source code is available at https://github.com/fristonio/gvm`
 var log *logger.Logger = logger.New(os.Stdout)
 
 var rootCmd = &cobra.Command{
-  Use:   "gvm",
-  Short: "gvm is a fast and reliable version manager for go",
-  Long:  longDescriptionGvm,
-  Run: func(cmd *cobra.Command, args []string) {
-    log.Warn("No arguments are supplied ... ")
-    cmd.Help()
-  },
+	Use:   "gvm",
+	Short: "gvm is a fast and reliable version manager for go",
+	Long:  longDescriptionGvm,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Warn("No arguments are supplied ... ")
+		cmd.Help()
+	},
 }
 
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func init() {
-  rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(versionCmd)
 }
